b81497f80a5219fd60423782a59c183a6cd8540e
nfabacus <n.fujioka@abacusmaths.info>
2020-06-02 22:01:43 +0100

    initial commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,49 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const setup = (props={}, state=null) => {
+  return shallow<App>(<App { ...props} />);
+};
+
+interface Wrapper {
+  find: (value: string) => any
+}
+
+const findByDataTestAttr = (wrapper: Wrapper, val: string) => {
+  return wrapper.find(`[data-test='${val}']`);
+};
+
+test('renders without error', () => {
+  const wrapper = setup();
+  const appComponent = findByDataTestAttr(wrapper, "component-app");
+  expect(appComponent.length).toBe(1);
 });
+
+test('renders increment button', () => {
+  const wrapper = setup();
+  const button = findByDataTestAttr(wrapper, "increment-button");
+  expect(button.length).toBe(1);
+});
+
+test('renders counter display', () => {
+  const wrapper = setup();
+  const counterDisplay = findByDataTestAttr(wrapper, "counter-display");
+  expect(counterDisplay.length).toBe(1);
+});
+
+test('counter starts at 0', () => {
+
+});
+
+test('clicking button increments counter display', () => {
+
+});
+
+
+
+// test('renders learn react link', () => {
+//   const wrapper = shallow(<App />);
+//   // console.log(wrapper.debug());
+//   expect(wrapper).toBeTruthy();
+// });
